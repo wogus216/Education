9ff589a158f041202d894eebb480ad20c5b7f860
권재현 <@DESKTOP-ALKPIF9>
2021-05-28 07:39:37 +0900

    교육 15일차

---
+++ Day_15/src/Aquariummain.java
@@ -0,0 +1,13 @@
+
+public class Aquariummain {
+
+	public static void main(String[] args) {
+		Aquarium fish = new Aquarium();
+		System.out.println("=================");
+		System.out.println("아쿠라리움 환영한다.");
+		System.out.println("=================");
+		
+		fish.run();
+	}
+
+}
